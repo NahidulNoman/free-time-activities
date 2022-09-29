df66eab53302b4f85c861d805e3a30ce3654bf33
NahidulNoman <nahidulna5@gmail.com>
2022-09-29 15:25:35 +0600

    responsive-done

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -5,11 +5,11 @@ const Questions = () => {
         <div className='mt-5'>
             <div className='container bg-light p-4 rounded-4 mb-3'>
                 <h4>How does react work ?</h4>
-                <p>React is a JavaScript library developed by Facebook. Its aim is to allow developers to easily create fast user interfaces for websites and applications alike. The main concept of React. js is virtual DOM.React reads these objects and uses them to create HTML elements on the virtual DOM, after which it gets synced with the real DOM. So we'll have trees of objects on the virtual DOM and trees of objects on the real DOM. React automatically updates the associated DOM element when we change data on a React element.</p>
+                <p className='opacity-75'>React is a JavaScript library developed by Facebook. Its aim is to allow developers to easily create fast user interfaces for websites and applications alike. The main concept of React. js is virtual DOM.React reads these objects and uses them to create HTML elements on the virtual DOM, after which it gets synced with the real DOM. So we'll have trees of objects on the virtual DOM and trees of objects on the real DOM. React automatically updates the associated DOM element when we change data on a React element.</p>
             </div>
             <div className='container bg-light p-4 rounded-4 mb-3'>
                 <h4>What is the difference between props and state in react ?</h4>
-                <p><span className='fw-bold'>Props :</span> <br />
+                <p className='opacity-75'><span className='fw-bold'>Props :</span> <br />
                 1.The Data is passed from one component to another.<br />
                 2.It is Immutable (cannot be modified).<br />
                 3.Props can be used with state and functional components.<br />
@@ -22,8 +22,9 @@ const Questions = () => {
                 4.State is both read and write.
                 </p>
             </div>
-            <div>
-                
+            <div className='container bg-light p-4 rounded-4 mb-3'>
+                <h4>uses of useEffect</h4>
+                <p className='opacity-75'>The useEffect Hook allows you to perform side effects in your components. This Hook, you tell React that your component needs to do something after render. React will remember the function you passed (we'll refer to it as our “effect”), and call it later after performing the DOM updates. In this effect, we set the document title, but we could also perform data fetching or call some other imperative API.</p>
             </div>
         </div>
     );
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -19,8 +19,8 @@ const Home = () => {
     }
 
     return (
-        <div className='row container-fluid '>
-            <div className='  col-9 '>
+        <div className='row container-fluid flex-column-reverse flex-lg-row'>
+            <div className='col-lg-9 col-sm-12'>
             <div className='bg-white p-3 shadow-lg rounded-3 mt-3 mb-4'>
                 <h4 className='m-5 fw-bold'><i className="fa-solid fa-timeline me-2"></i>  MY FREE TIME ROUTINE</h4>
                 <p className='mt-5 ms-5 fw-semibold'>You can see my free time activities below --</p>
@@ -35,7 +35,7 @@ const Home = () => {
                 }
                 </div>
             </div>
-            <div className='col-3'>
+            <div className='col-lg-3 col-sm-12'>
                     <SideBar
                     sideBar={sideBar}
                     ></SideBar>
